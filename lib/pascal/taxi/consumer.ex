25810bf3533f307a71003e23a892f2e1a950b5a5
Erik Perkins <erik.andrew.perkins@gmail.com>
2023-03-30 18:43:08 -0700

    Stream Kafka events over websocket

--- lib/pascal/taxi/consumer.ex
+++ lib/pascal/taxi/consumer.ex
@@ -1,6 +1,10 @@
 defmodule Pascal.Taxi.Consumer do
+  alias PascalWeb.Endpoint
+
   def handle_messages(messages) do
     for %{key: key, value: value} = message <- messages do
+      Endpoint.broadcast("stream:taxi", "trip", message)
+
       IO.inspect message
       IO.puts "#{key}: #{value}"
     end
